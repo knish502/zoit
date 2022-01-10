9e81ab606e316945dc42fa7b265ffef9f16bc169
knish502 <40375387+knish502@users.noreply.github.com>
2022-01-10 11:08:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 loc=$(pwd)
-sudo apt install python3 python3-pip firefox -y
+sudo apt install python3 python3-pip firefox ssmtp -y
 pip3 install selenium==4.0.0
 tar xzvf geckodriver*
 mkdir ~/geckodriver
@@ -11,3 +11,8 @@ cd $loc
 sudo mv gpubot /usr/bin/
 mkdir ~/PythonProjects
 mv bot.py ~/PythonProjects
+
+# see the following to set up ssmtp and epel-release
+# guides are for CENTOS but Ubuntu instructions are similar
+# https://www.linuxcnf.com/2018/03/how-to-install-epel-repository-on.html
+# https://www.linuxcnf.com/2018/11/how-to-install-and-configure-ssmtp-in.html
